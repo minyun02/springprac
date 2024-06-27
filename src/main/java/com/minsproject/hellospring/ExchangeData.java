a7b1c400429fe4f1e6504e8854d9e1baca5a0c9b
min <www5981@naver.com>
2024-06-27 22:49:06 +0900

    Payment prepare 요구사항에 만족하는 개발

---
+++ src/main/java/com/minsproject/hellospring/ExchangeData.java
@@ -0,0 +1,10 @@
+package com.minsproject.hellospring;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExchangeData(String result, Map<String, BigDecimal> rates) {
+}
